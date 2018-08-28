65fae8f02cf98e3de85c860188973acbde6727a2
Oldwolf Pang <bypang1226@gmail.com>
2018-08-28 14:49:21 +0800

    First commit

---
+++ meta/config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by vaibhav on 31/3/18
+ */
+module.exports = {
+  siteTitle: 'Meterialize Billing', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'A Meter Billing Software.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
+  userName: 'Vaibhav Sharma',
+  userTwitter: 'vaibhaved',
+  userLocation: 'Kuala Lumpur, Malaysia',
+  userDescription: '',
+  copyright: 'Copyright © Wolftek Technology 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
